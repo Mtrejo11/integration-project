55c6c05357aed1bfa6b193721292200841125357
mtrejo11 <mauriciotrejo.013@gmail.com>
2020-11-11 21:44:28 -0600

    Gulp fixes

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,14 @@
 let gulp = require("gulp");
 let jshint = require("gulp-jshint");
 
-gulp.task("default", async () => gulp.series("jshint", "test", "serve"));
+gulp.task("default", () => {
+  return Promise.resolve(gulp.series("jshint", "test", "serve"));
+});
 gulp.task("jshint", () => {
   return gulp.src("./*.js").pipe(jshint()).pipe(jshint.reporter("default"));
 });
 gulp.task("test", () => {
-  require("./test.js");
+  return Promise.resolve(require("./test.js"));
 });
 gulp.task("serve", () => {
   require("./main.js");
