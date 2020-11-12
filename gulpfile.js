d5b3ef17e8be6d8a122693a94dbf2686d99ccdbd
mtrejo11 <mauriciotrejo.013@gmail.com>
2020-11-11 22:40:42 -0600

    Gulp configured correctly

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,18 @@
 let gulp = require("gulp");
 let jshint = require("gulp-jshint");
 
-gulp.task("default", () => {
-  return Promise.resolve(gulp.series("jshint", "test", "serve"));
+gulp.task("jshint", (done) => {
+  gulp.src("./*.js").pipe(jshint()).pipe(jshint.reporter("default"));
+  done();
 });
-gulp.task("jshint", () => {
-  return gulp.src("./*.js").pipe(jshint()).pipe(jshint.reporter("default"));
-});
-gulp.task("test", () => {
-  return Promise.resolve(require("./test.js"));
+gulp.task("test", (done) => {
+  require("./test.js");
+  done();
 });
 gulp.task("serve", () => {
   require("./main.js");
+});
+gulp.task("default", gulp.series("jshint", "test", "serve"), (done) => {
+  console.log("FINISHED");
+  done();
 });
