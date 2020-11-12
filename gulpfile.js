9a999225807347a9984f7502c6aef20ff24a7764
mtrejo11 <mauriciotrejo.013@gmail.com>
2020-11-11 21:36:38 -0600

    gulp upgraded

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,15 @@
 "use strict";
 
-let gulp = require('gulp');
-let jshint = require('gulp-jshint');
+let gulp = require("gulp");
+let jshint = require("gulp-jshint");
 
-gulp.task('default',['jshint','test','serve']);
-
-gulp.task('jshint',()=>{
-	return gulp.src('./*.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'));
-})
-.task('test',()=>{
-	require('./test.js');
-})
-.task('serve',()=>{
-	require('./main.js');
+gulp.task("default", async () => gulp.series("jshint", "test", "serve"));
+gulp.task("jshint", () => {
+  return gulp.src("./*.js").pipe(jshint()).pipe(jshint.reporter("default"));
+});
+gulp.task("test", () => {
+  require("./test.js");
+});
+gulp.task("serve", () => {
+  require("./main.js");
 });
